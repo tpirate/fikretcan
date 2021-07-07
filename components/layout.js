7e967548da769c0e444617a511585d563eca464a
Fikret Can Gürbüz <70349622+tpirate@users.noreply.github.com>
2021-07-07 13:03:32 +0300

    change title :O

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Can Gurbuz'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Hello owo'
 
 export default function Layout({ children, home }) {
   return (
